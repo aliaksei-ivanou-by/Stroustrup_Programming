afce4bf2199b8f5ec72653420e506da047b0bb68
Aliaksei Ivanou <68548528+aliaksei-ivanou-by@users.noreply.github.com>
2020-07-20 20:17:25 +0300

    Book/Chapter_02

---
+++ Book/Chapter_02/Book_02_01.cpp
@@ -0,0 +1,7 @@
+// Эта программа выводит на экран сообщение "Hello, World!"
+#include "iostream"
+int main() // Программы на С++ начинаются с выполнения функции main
+{
+	std::cout<<"Hello, World!\n"; // Вывод "Hello, World!"
+	return 0;
+}
