005df9ad0f2ac33651c280b426c7213f73305a0d
Aliaksei Ivanou <aliaksei.ivanou.by@icloud.com>
2020-12-18 09:10:16 +0300

    Book/Chapter_20

--- Book/Chapter_20/Book_20_02.cpp
+++ Book/Chapter_20/Book_20_02.cpp
@@ -4,7 +4,7 @@ vector<double>* get_from_jill();
 
 void fct()
 {
-	ubt jack_count = 0;
+	int jack_count = 0;
 	double* jack_data = get_from_jack(&jack_count);
 	vector<double>* jill_data = get_from_jill();
 	// ...
