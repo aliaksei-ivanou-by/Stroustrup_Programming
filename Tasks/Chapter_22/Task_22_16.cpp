77c9bbaea09d0b85b9738b4cbdd5037cccb23ee4
Aliaksei Ivanou <aliaksei.ivanou.by@icloud.com>
2021-01-05 12:11:45 +0300

    Tasks/Chapter_22

---
+++ Tasks/Chapter_22/Task_22_16.cpp
@@ -0,0 +1,58 @@
+#ifndef TASK_22_16_STDAFX_H
+#include "Task_22_16_stdafx.h"
+#endif
+
+int main()
+try
+{
+	std::string fileName = "Task_22_16.txt";
+	std::map<std::string, ProgrammingLanguages> data = fileOpen(fileName);
+	std::pair<int, int> dataYearRange = getYearRange(data);
+
+	double delta = (dataYearRange.second - dataYearRange.first) / static_cast<double>(nTicks);
+	std::ostringstream oss;
+	for (int i = 0; i <= nTicks; ++i)
+	{
+		oss << std::left << std::setw(10) << int(dataYearRange.first + i * delta);
+	}
+	std::string labelX = oss.str();
+
+	Simple_window win(Point(50, 50), maxX, maxY, "Programming languages");
+	
+	Axis axisX(Axis::x, Point(offsetX, maxY - offsetY), lengthX, nTicks, labelX);
+	axisX.label.move(-maxX / 3.5, 0);
+	axisX.set_color(Color::black);
+	win.attach(axisX);
+
+	Vector_ref<Text> labels;
+	std::vector<std::string> labelsString;
+
+	for (auto i = data.begin(); i != data.end(); ++i)
+	{
+		double ratio = (i->second.getYear() - dataYearRange.first) / 
+			static_cast<double>(dataYearRange.second - dataYearRange.first);
+		int x = offsetX + ratio * (maxX - 2 * offsetX);
+		Text* label = new Text{ Point{ x, maxY - 2 * offsetY }, i->first };
+		updateAxisY(*label, labels);
+		label->set_color(Color::red);
+		labels.push_back(label);
+		labelsString.push_back(label->label());
+		win.attach(labels[labels.size() - 1]);
+	}
+
+	Vector_ref<Arrow> arrows;
+	
+	setArrows(win, data, labels, labelsString, arrows);
+
+	win.wait_for_button();
+	
+}
+catch (const std::exception& e)
+{
+	std::cout << "Exception occured: " << e.what() << '\n';
+	return 1;
+}
+catch (...)
+{
+	return 2;
+}
