27bbac7c0d063f5af509912c9714d1171c2125d7
Aliaksei Ivanou <aliaksei.ivanou.by@icloud.com>
2021-01-21 08:54:54 +0300

    Tasks/Chapter_26

---
+++ Tasks/Chapter_26/Task_26_04.cpp
@@ -0,0 +1,24 @@
+#ifndef TASK_26_04_STDAFX_H
+#include "Task_26_04_stdafx.h"
+#endif
+
+int main()
+try
+{
+	std::string fileNameSeq{ "Task_26_04_int.txt" };
+	std::string fileNameTest{ "Task_26_04_int_test.txt" };
+	std::cout << fileNameSeq << '\n';
+	std::ifstream inputFileStreamSeq{ fileNameSeq };
+	std::ifstream inputFileStreamTest{ fileNameTest };
+	unsigned int errors = test<int>(inputFileStreamSeq, inputFileStreamTest);
+	std::cout << "Errors: " << errors << '\n';
+}
+catch (const std::exception& e)
+{
+	std::cout << "Exception occured: " << e.what() << '\n';
+	return 1;
+}
+catch (...)
+{
+	return 2;
+}
